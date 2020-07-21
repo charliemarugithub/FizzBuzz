4191b321b0bf488878f2cc6a214a43aa09e9a5a6
Charlie Maru <charliemaru@gmail.com>
2020-07-21 20:35:17 +1200

    updating app.py

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from function import fizz_buzz
 
 root = Tk()
 
-myLabel = Label(root, text="FIZZBUZZ")
-clearLine = Label(root, text="")
-button = Button(root, text="Click Me!")
 
-clearLine.grid(row=0, column=0)
-myLabel.grid(row=1, column=0)
-button.grid(row=1, column=3, padx=20)
+def clickMe():
+    myLabel = Label(root, text="Look! I clicked a button!")
+    myLabel.pack()
+
+
+myButton = Button(root, text="Click Me!", command=clickMe)
+myButton.pack()
 
 root.mainloop()
 
