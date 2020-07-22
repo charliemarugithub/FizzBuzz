30e8669a4ada5b5dbefcbe128d21307f2a1281b0
Charlie Maru <charliemaru@gmail.com>
2020-07-22 17:46:41 +1200

    Last update till I learn more about tkinter

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from tkinter import *
-from function import fizz_buzz
 
 
 root = Tk()
 
+HEIGHT = 300
+WIDTH = 400
 
-def clickMe():
-    myLabel = Label(root, text="Look! I clicked a button!")
-    myLabel.pack()
+canvas = Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+mainLabel = Label(root, text='FizzBuzz Program')
+clearLine = Label(root, text='')
+descriptionLabel1 = Label(root, text="if Input is divisable by 3, then Fizz will be displayed")
+descriptionLabel2 = Label(root, text="if Input is divisable by 5, then Buzz will be displayed")
+descriptionLabel3 = Label(root, text="if Input is divisable by 3 & 5, then FizzBuzz will be displayed")
+entryField = Entry(root, text='Enter number here!')
+myButton = Button(root, text="Calculate!!")
+answerLabel = Label(root, text='answer here')
 
 
-myButton = Button(root, text="Click Me!", command=clickMe, fg="blue", bg="white")
 myButton.pack()
+entryField.pack()
+descriptionLabel1.pack()
+descriptionLabel2.pack()
+descriptionLabel3.pack()
+answerLabel.pack()
 
 root.mainloop()
 
